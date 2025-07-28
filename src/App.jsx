def511037543977c2d4e43d4f6119d8ea5ccda49
abdul8199 <2400030294@kluniversity.in>
2025-07-28 16:15:07 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./App.css"
+class App extends Component {
+  render() {
+    return (
+      <div className='mystyle'>
+        <h1>COMPUTER SCIENCE ENGINEERING</h1>
+        <h2>FRONT END DEVELOPMENT FRAMEWORKS</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
